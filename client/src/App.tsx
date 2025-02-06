295137c55a5a8d11838a790667d26ad421ee72bd
Vikash kumar <vikash08.kumar1995@gmail.com>
2025-02-06 14:35:05 +0530

    initial setup to get started

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+
+import { CssBaseline } from '@mui/material'
+
+function App() {
+  return (
+    <div className="App">
+      <CssBaseline />
+      <h1>Hello WORLD</h1>
+    </div>
+  )
+}
+
+export default App
