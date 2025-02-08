aa58012ca26b559bb6ac6cca1c4f68107d7138f5
Vikash kumar <vikash08.kumar1995@gmail.com>
2025-02-09 00:46:00 +0530

    routes setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,37 @@
-
 import { CssBaseline } from '@mui/material'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { routes } from './routes'
+import Home from './pages/Home/Home'
+import Dashboard from './pages/Dashboard/Dashboard'
+import Profile from './pages/Profile/Profile'
+import About from './pages/About/About'
+import Login from './pages/Login/Login'
+
+const router = createBrowserRouter([
+  {
+    path: routes.home,
+    element: <Home />
+  }, {
+    path: routes.login,
+    element: <Login />
+  }
+  , {
+    path: routes.dashboard,
+    element: <Dashboard />
+  }, {
+    path: routes.profile,
+    element: <Profile />
+  }, {
+    path: routes.about,
+    element: <About />
+  }
+])
 
 function App() {
   return (
     <div className="App">
       <CssBaseline />
-      <h1>Hello WORLD</h1>
+      <RouterProvider router={router} />
     </div>
   )
 }
---
+++ client/src/routes.ts
@@ -0,0 +1,11 @@
+export const routes ={
+    home:'/',
+    login:'/login',
+    register:'/register',
+    dashboard:'/dashboard',
+    profile:'/profile',
+    settings:'/settings',
+    logout:'/logout',
+    notFound:'/404',
+    about:"/about"
+}
